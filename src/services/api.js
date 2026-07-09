6d57cf836b67f3758623916dc849f01a8ac7d787
vishalsingh455 <thevishalsingh79@gmail.com>
2026-07-09 12:24:15 +0530

    docker execution

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,21 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//     baseURL: "https://codo-backend.vercel.app/api",
+//     withCredentials: true,
+//     headers: {
+//         "Content-Type": "application/json"
+//     }
+// });
+
+// export default api;
+
+
 import axios from "axios";
 
+// This will automatically look for the variable you just saved in Vercel!
 const api = axios.create({
-    baseURL: "https://codo-backend.vercel.app/api",
+    baseURL: process.env.REACT_APP_API_URL || process.env.VITE_API_URL || "http://16.171.200.174:3000",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
